db14e7d97e6ac05da8bebb8b1d731c6ea770f8f1
Grant <acdcgrr@gmail.com>
2024-03-24 16:32:05 -0500

    added explicit =default to special member functions of StaticArray and added rvalue overload to fill()

--- ArraysWithoutIterator/StaticArrayWithoutIterator.h
+++ ArraysWithoutIterator/StaticArrayWithoutIterator.h
@@ -9,21 +9,35 @@ class StaticArray
 {
 private:
     T elems[size];
+
 public:
     constexpr size_t getSize() const {return size;}
 
     T& operator[](size_t index) {return elems[index];}
     const T& operator[](size_t index) const {return elems[index];}
 
+    StaticArray() = default;
+
+    //rule of zero
+    ~StaticArray() = default;
+    StaticArray(StaticArray&&) = default;
+    StaticArray(const StaticArray&) = default;
+    StaticArray& operator=(StaticArray&&) = default
+    StaticArray& operator=(const StaticArray&) = default;
+
     //helper method to fill the array with a specific value
     void fill(const T& value) 
     {
-        for(int i = 0; i < size; i++)
-        {
+        for(int i = 0; i < size; ++i)
             elems[i] = value;
-        }
     }
 
-    T* data() {return elems;}
-    const T* data() const {return elems;}
+    void fill(T&& value)
+    {
+        for(int i = 0; i < size; ++i)
+            elems[i] = std::move(value);
+    }
+
+    T* rawData() {return elems;}
+    const T* rawData() const {return elems;}
 };
